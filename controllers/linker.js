509b49ae2f7a367abc37e7eae8703884e423b93f
Grant_Nathaniel_Brown <gnb225@nyu.edu>
2018-02-22 16:36:25 -0500

    made form for submitting new questions, it doesn't work yet.

--- controllers/linker.js
+++ controllers/linker.js
@@ -1,16 +1,25 @@
 const express = require('express')
 const router = express.Router()
 
-const Models = require('./models/qanda')
+const Models = require('../models/qanda')
 const Question = Models.Question
 const Answer = Models.Answer
 
 // localhost:3000/questions
-router.get("/questions", (req, res) =>{
+router.get("/", (req, res) =>{
     res.render('../views/pages/new')
 })
+
+router.post('/', (req, res) => {
+    console.log(req.body.title)
+})
+// router.get('/new', (req, res) => {
+//     res.render('pages/new')
+// })
 
 // 1. link to /questions/new that render new.hbs
 // 2. add form to new.hbs
 // 3. make it so form submits to route in controller
 // 4. make it so route saves new question to DB
+
+module.exports = router
--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const Answer = Models.Answer
 
 const app = express();
 
+const linkerController = require('./controllers/linker')
+// app.use('Question')
 app.set("view engine", "hbs")
 
 app.get('/', (req, res) => {
@@ -15,9 +17,13 @@ app.get('/', (req, res) => {
             res.render('index', { questions })
         })
 });
+
+
 //question.find({}) grabs all of the ?documents? in the model
 //.then function
 //renders the index.hbs page, with questions object
+
+app.use("/questions", linkerController)
 
 app.listen(3000, () => {
     console.log('the test worked');
